6b3c9fc232b0a382de5c5fdce695cb87c5f7e41c
Nir Duan <nir.duan@motorolasolutions.com>
2017-08-17 20:20:37 +0300

    8.3.1

--- base.asm
+++ base.asm
@@ -6,17 +6,21 @@ DATASEG
 ; Your variables here
 ; --------------------------
 address dw 000Eh
+value dw 5
+
 CODESEG
 start:
 	mov ax, @data
 	mov ds, ax
-	
-	;----------------------------------------------------
-	xor ax,ax
-	jmp [address]
-	add ax,5
-	add ax,4
+	;--------------------  CODE STARTS HERE --------------------------------
 
+	mov ax,[value]
+	cmp ax,0
+	jle AfterdecAxByOn
+	dec ax
+							AfterdecAxByOn:
+	
+	
 exit:
 	mov ax, 4c00h
 	int 21h
