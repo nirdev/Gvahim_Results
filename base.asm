ee583008186937073d0212351c5e0bfaf5c1c5bb
Nir Duan <nir.duan@motorolasolutions.com>
2017-08-19 10:40:27 +0300

    8.3.4

--- base.asm
+++ base.asm
@@ -6,22 +6,31 @@ DATASEG
 ; Your variables here
 ; --------------------------
 address dw 000Eh
-AxValue dw 5
-BxValue dw 5
+var1 db 5
+var2 db 6 
+BxValue dw 0
 
 CODESEG
 start:
 	mov ax, @data
 	mov ds, ax
 	;--------------------  CODE STARTS HERE --------------------------------
+	xor ax,ax
+	xor bx,bx
 
-	mov ax,[AxValue]
-	mov bx,[BxValue]
-	cmp ax,bx
-	je copyBxToAx
-	mov ax,bx
-							copyBxToAx:
+	mov bl,[var2]
+	mov al,[var1]
+		
+	cmp bl,al
+	je addVars
+	jne subVars
+			subVars:
+	sub ax,bx	
+	jmp finale
 	
+			addVars:
+	add ax,bx
+			finale:
 	
 exit:
 	mov ax, 4c00h
