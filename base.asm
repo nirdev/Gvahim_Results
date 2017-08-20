aace41904bdb0b0b8c7d2abbcbc8aeaecaf827e8
Nir Duan <nir.duan@motorolasolutions.com>
2017-08-20 19:44:57 +0300

    9.1.5

--- base.asm
+++ base.asm
@@ -1,69 +1,22 @@
 IDEAL
 MODEL small
-STACK 100h
+STACK 20h
 DATASEG
 ; -------------------------- 
 ; Your variables here
 ; --------------------------
-h db ?
-l db ?
-sum db 0
 CODESEG
 start:
 	mov ax, @data
 	mov ds, ax
 	;--------------------  CODE STARTS HERE --------------------------------
-	xor cx,cx
-	xor si,si
+
 	xor ax,ax
 	xor bx,bx
-	
-	;get hight
-	mov ah,1h
-	int 21h
-	sub al,'0'
-	mov [h], al
+	mov ax, 1234h
+	push ax
+	pop bx
 
-	;get length	
-	mov ah,1h
-	int 21h
-	sub al,'0'
-	mov [l],al
-	
-	;/n
-	mov dl,0ah
-	mov ah,2h
-	int 21h
-	
-
-	;Set outer loop indexes (cx for inner)
-	mov bl,[h]
-			outerLoop:
-	mov cl,[l]
-			innerLoop:
-	
-	mov dl,'x'
-	mov ah,2h
-	int 21h
-
-	loop innerLoop
-	;/n
-	mov dl,0ah
-	mov ah,2h
-	int 21h
-	
-	;outer looping
-	dec bx
-	cmp bx,0
-	jne outerLoop
-
-	;finished
-	;get length	
-	mov ah,1h
-	int 21h
-	sub al,'0'
-	mov [l],al
-	
 exit:
 	mov ax, 4c00h
 	int 21h
