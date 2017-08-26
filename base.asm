bc30240a6cad06ea5a4c161044e17741ad55dfe3
Nir Duan <nir.duan@motorolasolutions.com>
2017-08-26 10:00:30 +0300

    9.6.1 - Correct Usage of BP as Stack Reference

--- base.asm
+++ base.asm
@@ -4,7 +4,11 @@ STACK 100h
 DATASEG
   	;--------------------  YOUR VARIABLES HERE -------------------  
 retAdd dw ?
-var1 dw 33h
+var1 dw 1h
+var2 dw 2h
+
+var1old dw ?
+var2old dw ?
 
 CODESEG
 
@@ -12,13 +16,38 @@ CODESEG
 	;--------------------  PROCEDURE STARTS HERE --------------------------------
 
 proc incPbR
+	
+	push bp
+	mov bp,sp
+	
+	xor ax,ax
+	xor bx,bx
+	xor cx,cx
+	xor dx,dx
+	
+	;save var1 original value to local pramater
+	mov bx,[bp+6]
+	mov ax,[word ptr ds:bx]
+	mov [var1old],ax
 
-	pop [retAdd] ;save ret address
-	pop bx ;save parm ref
+	;save var2 original value to local pramater
+	mov bx,[bp+4]
+	mov ax,[word ptr ds:bx]
+	mov [var2old],ax
+	
+	;move value of var2 to var1
+	mov bx,[bp+6]
+	mov ax,[var2old]
+	mov [word ptr ds:bx],ax
 
-	add [byte ptr ds:bx],1
+	;move value of var1 to var2
+	mov bx,[bp+4]
+	mov ax,[var1old]
+	mov [word ptr ds:bx],ax
+	
 
-	push [retAdd]
+
+	pop bp
 	ret
 endp incPbR
 
@@ -28,6 +57,7 @@ start:
 	;--------------------  CODE STARTS HERE --------------------------------
 
 	push offset var1 
+	push offset var2 
 	call incPbR
 exit:
 	mov ax, 4c00h
